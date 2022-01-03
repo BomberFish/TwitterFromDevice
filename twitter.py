b275b31937795526b391c667ae44bcaf17b41dac
DALEK-77 <56525810+DALEK-77@users.noreply.github.com>
2022-01-03 12:05:42 -0500

    do the thing

---
+++ twitter.py
@@ -0,0 +1,15 @@
+import tweepy
+
+# init
+consumer_key = open(consumer_key, "r")
+consumer_secret = open(consumer_secret, "r")
+access_token = open(access_token, "r")
+access_token_secret = open(access_token_secret, "r")
+
+# haha oauth go brrr
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitter = tweepy.API(auth)
+
+# Ask questions
+tweet = input("Enter your tweet: ")
